e8ee0efcea50883a3590828a5607d973ed647516
Rulo Kobashikawa <akobashikawa@gmail.com>
2017-05-29 12:43:36 -0500

    Prevent special chars in search query
    Add jquery to vendors

--- src/scripts/search-form.js
+++ src/scripts/search-form.js
@@ -1,3 +1,12 @@
-function searchForm(){
-    // code here
+$('.search-form').on('submit', function(event) {
+    event.preventDefault();
+    var q = $('.search-input').val();
+    if ( /[$%\/\\]/.test(q) ) {
+        console.log('invalid char in search');
+        return false;
+    }
+    console.log('search string ok');
+});
+
+function searchForm(event){
 }
